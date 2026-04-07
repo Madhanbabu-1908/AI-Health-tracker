5fe9a8ae469a0e38f17cfd69ac20f14b13d724fa
Madhanbabu-1908 <89144800+Madhanbabu-1908@users.noreply.github.com>
2026-04-07 11:47:59 +0530

    Create capacitor.config.ts

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,14 @@
+{
+  "appId": "com.madhan.healthtracker",
+  "appName": "Madhan Health Tracker",
+  "webDir": "dist",
+  "server": {
+    "androidScheme": "https",
+    "cleartext": true
+  },
+  "android": {
+    "allowMixedContent": true,
+    "minSdkVersion": 23,
+    "targetSdkVersion": 33
+  }
+}
